81f2641c9b309823cf44528a5fa7011d83a67aec
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2026-06-20 01:09:27 +0300

    luxcareers

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /home/project/app/page.tsx
+// File: C:\Users\KHALED\Desktop\anonymous\luxcareers\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
